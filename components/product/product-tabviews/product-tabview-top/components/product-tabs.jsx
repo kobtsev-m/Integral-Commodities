846d7e9f46045891af5fc61f473936ed6d336a2a
Артем Черенков <art.cherenkov@gmail.com>
2021-06-22 15:11:10 +0700

    IC-98 feat: add smooth transition for "Prices", "Offer" and so tabs

--- components/product/product-tabviews/product-tabview-top/components/product-tabs.jsx
+++ components/product/product-tabviews/product-tabview-top/components/product-tabs.jsx
@@ -2,6 +2,8 @@ import { nanoid } from "nanoid";
 
 import cn from "classnames";
 import styles from "./product-tabs.module.css";
+import { useEffect, useRef, useState } from "react";
+import useWindowDimensions from "../../../../../hooks/useWindowDemensions";
 
 const polymersTabs = {
   Prices: "prices",
@@ -16,11 +18,50 @@ const otherTypesTabs = {
   Product: "product",
 };
 
+const setClippingRect = (element, width, left) => {
+  element.style.width = width + "px";
+  element.style.left = left + "px";
+};
+
 function ProductTabs(props) {
   const tabs = props.category === "polymers" ? polymersTabs : otherTypesTabs;
+  const tabsElements = useRef();
+  const activeTabLine = useRef();
+  const [firstElementOffset, setFirstElementOffset] = useState(0);
+  const { width } = useWindowDimensions();
+
+  useEffect(() => {
+    const tabs = tabsElements.current.children;
+    setFirstElementOffset(tabs[0].getBoundingClientRect().x);
+  }, [width]);
+
+  useEffect(() => {
+    const activeTab = tabsElements.current
+      .getElementsByClassName(styles.productTabs__item_active)[0]
+      .getBoundingClientRect();
+
+    setClippingRect(
+      activeTabLine.current,
+      activeTab.width,
+      activeTab.left - firstElementOffset
+    );
+  }, [width, firstElementOffset, props.activeTab]);
+
   return (
     <div className={cn(styles.productTabs__listWrapper, "sticky-top")}>
-      <ul className={styles.productTabs__list}>
+      <span
+        ref={activeTabLine}
+        style={{
+          width: 100,
+          height: 2,
+          position: "absolute",
+          backgroundColor: "var(--p-blue)",
+          bottom: 0,
+          left: 0,
+          transition: "200ms ease",
+        }}
+      />
+      <ul className={styles.productTabs__list} ref={tabsElements}>
         {Object.entries(tabs).map(([tabLabel, tabName]) => (
           <li
             key={nanoid()}
